c5c637293229bde17916f154d80d7d3615d94936
Adrian <almondboy246@gmail.com>
2024-02-16 13:08:06 -0800

    add comments

--- data.py
+++ data.py
@@ -7,24 +7,26 @@ import json
 
 UNIQUE_VARIATIONS = {'stop_gained', 'protein_altering_variant', 'missense_variant'}
 
+# Open and read file
 file = open("gwas-association-downloaded_2024-02-07-EFO_0009443-withChildTraits_Breast-Cancer.tsv")
+tsv_file = csv.reader(file, delimiter="\t")
 matching_data = []
 
-tsv_file = csv.reader(file, delimiter="\t")
 
+# Useful table columns and what they contain, only applie to GWAS catalog
 # index 24 = CONTEXT
 # index 14 = MAPPED_GENE
 # index 21 = SNPS
 
+# Reduce file to only usable types of mutations
 for line in tsv_file:
     if line[24] in UNIQUE_VARIATIONS:
         matching_data.append(line)
         # print(line[14])
         # print(line[21])
 
-
+# Get variation and convert to usable format
 request = requests.get("https://clinicaltables.nlm.nih.gov/api/snps/v3/search", params={'terms': 'rs16991615', 'maxList': 20})
-
 res = json.loads(request.text)
 
 print(res[3])
